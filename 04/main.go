cea402e92dbcbe994735bc90d6bf9306a7a3aa85
Lee Martin <hello@leejohnmartin.co.uk>
2023-12-04 20:44:10 +0000

    04-pt2

--- 04/main.go
+++ 04/main.go
@@ -30,6 +30,39 @@ func (scrd Scratchcard) ScoreScratchcard() int {
 	return cuml
 }
 
+func (scrd Scratchcard) CardIndexesWon() []int {
+	cuml := []int{}
+	for _, wm := range scrd.WinningNumbers {
+		for _, cn := range scrd.CardNumbers {
+			if cn == wm {
+				cuml = append(cuml, len(cuml)+scrd.CardNumber)
+				continue
+			}
+		}
+	}
+	return cuml
+}
+
+// this is hilariously slow and brute force
+func CollectCountOfCards(cards [][]Scratchcard, index int, culm int) ([][]Scratchcard, int) {
+	if index >= len(cards) {
+		return cards, culm
+	}
+	icards := cards[index]
+
+	for _, crd := range icards {
+		culm = culm + 1
+		indexes := crd.CardIndexesWon()
+		for _, val := range indexes {
+			if val < len(cards) {
+				cards[val] = append(cards[val], cards[val][0])
+			}
+		}
+	}
+
+	return CollectCountOfCards(cards, index+1, culm)
+}
+
 func ParseScratchcard(input string) (Scratchcard, error) {
 	scard := Scratchcard{}
 	splt := strings.Split(input, ":")
@@ -70,11 +103,15 @@ func main() {
 	buf, _ := os.ReadFile("data.txt")
 	input := string(buf)
 	cards := []Scratchcard{}
+	multiplyingcards := [][]Scratchcard{}
 	total := 0
 	for _, crd := range strings.Split(input, "\n") {
 		cr, _ := ParseScratchcard(crd)
 		cards = append(cards, cr)
+		multiplyingcards = append(multiplyingcards, []Scratchcard{cr})
 		total = total + cr.ScoreScratchcard()
 	}
 	fmt.Printf("Part One Total: %d\n", total)
+	_, multitotal := CollectCountOfCards(multiplyingcards, 0, 0)
+	fmt.Printf("Part Two Total: %d\n", multitotal)
 }
--- 04/main_test.go
+++ 04/main_test.go
@@ -51,3 +51,27 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 		t.Errorf("Expected 13 | Got %v", total)
 	}
 }
+
+func TestIntegrationWithBothPt2(t *testing.T) {
+	input := `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
+	cards := [][]Scratchcard{}
+	for _, crd := range strings.Split(input, "\n") {
+		cr, _ := ParseScratchcard(crd)
+		cards = append(cards, []Scratchcard{cr})
+	}
+
+	_, total := CollectCountOfCards(cards, 0, 0)
+
+	// for i, cal := range culm {
+	// 	fmt.Printf("index: %d | count %d\n", i, len(cal))
+	// }
+
+	if 30 != total {
+		t.Errorf("Expected 30 | Got %v", total)
+	}
+}
