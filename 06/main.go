e1c78ca8dc60cbdad39e3be87ee35faaa1e9e2ce
Lee Martin <hello@leejohnmartin.co.uk>
2023-12-06 22:13:20 +0000

    06-pt1

---
+++ 06/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type Race struct {
+	Duration int
+	Record   int
+}
+
+func (rc Race) GetVictoryBounds() (int, int) {
+	// work out lowest
+	lower := -1
+	i := 0
+	for lower < 0 {
+		if (rc.Duration-i)*i > rc.Record {
+			lower = i
+		}
+		i = i + 1
+	}
+	// work out highest
+	upper := -1
+	i = rc.Duration
+	for upper < 0 {
+		if (rc.Duration-i)*i > rc.Record {
+			upper = i
+		}
+		i = i - 1
+	}
+	return lower, upper
+}
+
+func ParseRaces(input string) []Race {
+	lines := strings.Split(input, "\n")
+	durations := []int{}
+	records := []int{}
+
+	buffer := ""
+
+	for _, chr := range lines[0] {
+		if unicode.IsNumber(chr) {
+			buffer = buffer + string(chr)
+		} else if len(buffer) > 0 {
+			i, _ := strconv.Atoi(buffer)
+			durations = append(durations, i)
+			buffer = ""
+		}
+	}
+	if len(buffer) > 0 {
+		i, _ := strconv.Atoi(buffer)
+		durations = append(durations, i)
+		buffer = ""
+	}
+	for _, chr := range lines[1] {
+		if unicode.IsNumber(chr) {
+			buffer = buffer + string(chr)
+		} else if len(buffer) > 0 {
+			i, _ := strconv.Atoi(buffer)
+			records = append(records, i)
+			buffer = ""
+		}
+	}
+	if len(buffer) > 0 {
+		i, _ := strconv.Atoi(buffer)
+		records = append(records, i)
+		buffer = ""
+	}
+	races := []Race{}
+	for i, dur := range durations {
+		races = append(races, Race{
+			Duration: dur,
+			Record:   records[i],
+		})
+	}
+	return races
+}
+
+func main() {
+	buf, _ := os.ReadFile("data.txt")
+	stringput := string(buf)
+	res := ParseRaces(stringput)
+
+	total := 1
+
+	for _, rc := range res {
+		lower, upper := rc.GetVictoryBounds()
+		total = total * (upper - lower + 1)
+	}
+
+	fmt.Printf("Result: %v\n", total)
+}
---
+++ 06/main_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAssert(t *testing.T) {
+	input := `Time:      7  15   30
+Distance:  9  40  200`
+	res := ParseRaces(input)
+	expectedRaces := []Race{
+		{
+			Duration: 7,
+			Record:   9,
+		},
+		{
+			Duration: 15,
+			Record:   40,
+		},
+		{
+			Duration: 30,
+			Record:   200,
+		},
+	}
+	for i := range res {
+		if !reflect.DeepEqual(expectedRaces[i], res[i]) {
+			t.Errorf("Expected %v got %v", expectedRaces[i], res[i])
+		}
+	}
+}
+
+func TestGetVictoryBounds(t *testing.T) {
+	testRace := Race{
+		Duration: 7,
+		Record:   9,
+	}
+
+	lower, upper := testRace.GetVictoryBounds()
+
+	if lower != 2 {
+		t.Errorf("Expected 2 got %v", lower)
+	}
+	if upper != 5 {
+		t.Errorf("Expected 5 got %v", upper)
+	}
+}
+
+func TestExample(t *testing.T) {
+
+	input := `Time:      7  15   30
+Distance:  9  40  200`
+	res := ParseRaces(input)
+
+	total := 1
+
+	for _, rc := range res {
+		lower, upper := rc.GetVictoryBounds()
+		total = total * (upper - lower + 1)
+	}
+
+	if total != 288 {
+		t.Errorf("Expected 288 got %v", total)
+	}
+}
