a8188a765fe9d770a0b562b51969649b11589445
Lee Martin <hello@leejohnmartin.co.uk>
2023-12-10 14:01:40 +0000

    10-pt1

---
+++ 10/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Tile struct {
+	X    int
+	Y    int
+	Rune rune
+}
+
+type InputOutput struct {
+	X, Y int
+}
+
+func (tl Tile) ConnectsTo() []InputOutput {
+	// we return +/-1 of the directions of travel
+	// so, for example, []InputOutput{{-1, 0}, {1, 0}}
+	// is left-right on the X axis from {0,0}
+	res := []InputOutput{}
+	if tl.Rune == '|' {
+		res = append(res, []InputOutput{{tl.X, tl.Y - 1}, {tl.X, tl.Y + 1}}...)
+	}
+	if tl.Rune == '-' {
+		res = append(res, []InputOutput{{tl.X - 1, tl.Y}, {tl.X + 1, tl.Y}}...)
+	}
+	if tl.Rune == 'L' {
+		res = append(res, []InputOutput{{tl.X, tl.Y - 1}, {tl.X + 1, tl.Y}}...)
+	}
+	if tl.Rune == 'J' {
+		res = append(res, []InputOutput{{tl.X, tl.Y - 1}, {tl.X - 1, tl.Y}}...)
+	}
+	if tl.Rune == '7' {
+		res = append(res, []InputOutput{{tl.X, tl.Y + 1}, {tl.X - 1, tl.Y}}...)
+	}
+	if tl.Rune == 'F' {
+		res = append(res, []InputOutput{{tl.X, tl.Y + 1}, {tl.X + 1, tl.Y}}...)
+	}
+	return res
+}
+
+func ParseTiles(input string) (InputOutput, [][]Tile) {
+	tiles := [][]Tile{}
+	startPos := InputOutput{}
+	for y, ln := range strings.Split(input, "\n") {
+
+		for x, chr := range ln {
+			tile := Tile{Rune: chr, X: x, Y: y}
+			if y == 0 {
+				tiles = append(tiles, []Tile{tile})
+			} else {
+				tiles[x] = append(tiles[x], tile)
+			}
+			if chr == 'S' {
+				startPos = InputOutput{x, y}
+			}
+		}
+	}
+	return startPos, tiles
+}
+
+func includes(inputs []InputOutput, mtch InputOutput) bool {
+	for _, inp := range inputs {
+		if inp.X == mtch.X && inp.Y == mtch.Y {
+			return true
+		}
+	}
+	return false
+}
+
+func GetShuttlesAndRoutes(start InputOutput, tiles [][]Tile) ([]InputOutput, [][]InputOutput) {
+	shuttles := []InputOutput{}
+	routes := [][]InputOutput{}
+	// see which tiles around start can connect to start
+	for _, x := range []int{-1, 1, 0} {
+		for _, y := range []int{-1, 1, 0} {
+			if x == 0 && y == 0 {
+				continue
+			}
+			//ignore diagonals
+			if x != 0 && y != 0 {
+				continue
+			}
+			neighbourConnection := tiles[x+start.X][y+start.Y].ConnectsTo()
+			if includes(neighbourConnection, start) {
+				shuttles = append(shuttles, InputOutput{x + start.X, y + start.Y})
+				routes = append(routes, []InputOutput{{start.X, start.Y}})
+			}
+		}
+	}
+	return shuttles, routes
+}
+
+func RecurrAlongRoute(tiles [][]Tile, start InputOutput, shuttle InputOutput, route []InputOutput) (bool, []InputOutput) {
+
+	if shuttle.X == start.X && shuttle.Y == start.Y {
+		return true, route
+	}
+	lastLocation := route[len(route)-1]
+	currentConnections := tiles[shuttle.X][shuttle.Y].ConnectsTo()
+	nextConnection := lastLocation
+
+	for _, con := range currentConnections {
+		if con.X != nextConnection.X || con.Y != nextConnection.Y {
+			nextConnection = con
+			break
+		}
+	}
+	if nextConnection.X == lastLocation.X && nextConnection.Y == lastLocation.Y {
+		//dead end
+		return false, route
+	}
+	route = append(route, InputOutput{shuttle.X, shuttle.Y})
+
+	return RecurrAlongRoute(tiles, start, InputOutput{nextConnection.X, nextConnection.Y}, route)
+}
+
+func GetLengthOfLoop(start InputOutput, tiles [][]Tile) int {
+	shuttles, routes := GetShuttlesAndRoutes(start, tiles)
+	isLoop := []bool{}
+	for i, shuttle := range shuttles {
+		isLoop = append(isLoop, false)
+
+		isL, route := RecurrAlongRoute(tiles, start, shuttle, routes[i])
+		isLoop[i] = isL
+		routes[i] = route
+	}
+	count := 0
+	for i, success := range isLoop {
+		if success {
+			count = len(routes[i])
+			break
+		}
+	}
+	return count
+}
+
+func main() {
+	buf, _ := os.ReadFile("data.txt")
+	input := string(buf)
+	startPos, tiles := ParseTiles(input)
+	length := GetLengthOfLoop(startPos, tiles)
+	fmt.Printf("Result: %v\n", length/2)
+}
---
+++ 10/main_test.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTileParsing(t *testing.T) {
+	input := `.....
+.S-7.
+.|.|.
+.L-J.
+.....`
+	startPos, res := ParseTiles(input)
+	for y, ln := range strings.Split(input, "\n") {
+		for x, chr := range ln {
+
+			if chr != res[x][y].Rune {
+				t.Errorf("Expected %v got %v", chr, res)
+			}
+			if x != res[x][y].X {
+				t.Errorf("Expected %v got %v", x, res)
+			}
+			if y != res[x][y].Y {
+				t.Errorf("Expected %v got %v", y, res)
+			}
+		}
+	}
+	if startPos.X != 1 && startPos.Y != 1 {
+
+		t.Errorf("Expected %v got %v", InputOutput{1, 1}, startPos)
+	}
+}
+
+func TestPartOneRouting(t *testing.T) {
+	input := `.....
+.S-7.
+.|.|.
+.L-J.
+.....`
+	startPos, tiles := ParseTiles(input)
+	length := GetLengthOfLoop(startPos, tiles)
+	if length != 8 {
+
+		t.Errorf("Expected 8 got %v", length)
+	}
+}
+
+func TestShuttleGenerating(t *testing.T) {
+	input := `.....
+.S-7.
+.|.|.
+.L-J.
+.....`
+	startPos, tiles := ParseTiles(input)
+	shuttle, routes := GetShuttlesAndRoutes(startPos, tiles)
+	if shuttle[0].X != 2 && shuttle[0].Y != 1 {
+
+		t.Errorf("Expected asd got %v", shuttle[0])
+	}
+	if shuttle[1].X != 1 && shuttle[1].Y != 2 {
+
+		t.Errorf("Expected asd got %v", shuttle[0])
+	}
+	for i, rt := range routes {
+		if !matches(rt, []InputOutput{startPos}) {
+			t.Errorf("Expected %d got %v", i, rt)
+
+		}
+	}
+}
+
+func matches(one []InputOutput, two []InputOutput) bool {
+	contains := []bool{}
+	for _, o := range one {
+		for _, t := range two {
+			if o.X == t.X && o.Y == t.Y {
+				contains = append(contains, true)
+			}
+		}
+	}
+	return len(contains) == len(one) && len(one) == len(two)
+}
+
+func TestConnectsTo(t *testing.T) {
+	// | is a vertical pipe connecting north and south.
+	tile := Tile{
+		X:    0,
+		Y:    0,
+		Rune: '|',
+	}
+	expected := []InputOutput{{0, -1}, {0, 1}}
+	actual := tile.ConnectsTo()
+	if !matches(expected, actual) {
+
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// - is a horizontal pipe connecting east and west.
+	tile.Rune = '-'
+	expected = []InputOutput{{-1, 0}, {1, 0}}
+	actual = tile.ConnectsTo()
+	if !matches(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// L is a 90-degree bend connecting north and east.
+	tile.Rune = 'L'
+	expected = []InputOutput{{0, -1}, {1, 0}}
+	actual = tile.ConnectsTo()
+	if !matches(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// J is a 90-degree bend connecting north and west.
+	tile.Rune = 'J'
+	expected = []InputOutput{{0, -1}, {-1, 0}}
+	actual = tile.ConnectsTo()
+	if !matches(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// 7 is a 90-degree bend connecting south and west.
+	tile.Rune = '7'
+	expected = []InputOutput{{0, 1}, {-1, 0}}
+	actual = tile.ConnectsTo()
+	if !matches(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// F is a 90-degree bend connecting south and east.
+	tile.Rune = 'F'
+	expected = []InputOutput{{0, 1}, {1, 0}}
+	actual = tile.ConnectsTo()
+	if !matches(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// . is ground; there is no pipe in this tile.
+	tile.Rune = '.'
+	expected = []InputOutput{}
+	actual = tile.ConnectsTo()
+	if !matches(expected, actual) {
+		t.Errorf("Expected %v got %v", expected, actual)
+	}
+	// S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+}
