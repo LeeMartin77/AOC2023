d9d85148b6b3f9978d5def445bf3b06a9fb1f2da
Lee Martin <hello@leejohnmartin.co.uk>
2023-12-14 19:54:19 +0000

    14-pt2 WIP
    Individual rollings work, but spin doesn't

--- 14/main.go
+++ 14/main.go
@@ -16,7 +16,7 @@ var (
 	SquareRock = '#'
 )
 
-func ParseRocks(input string) []Rock {
+func ParseRocks(input string) ([]Rock, int, int) {
 	ret := []Rock{}
 	for y, ln := range strings.Split(input, "\n") {
 		for x, chr := range ln {
@@ -28,18 +28,23 @@ func ParseRocks(input string) []Rock {
 			}
 		}
 	}
-	return ret
-}
-
-func RollRocksNorth(inp []Rock) []Rock {
-	rocks := inp
-	columnIndexes := [][]int{}
 	maxX := 0
-	for _, rk := range rocks {
-		if maxX < rk.X {
+	maxY := 0
+	for _, rk := range ret {
+		if rk.X > maxX {
 			maxX = rk.X
 		}
+
+		if rk.Y > maxY {
+			maxY = rk.Y
+		}
 	}
+	return ret, maxX, maxY
+}
+
+func RollRocksNorth(inp []Rock, maxX int, maxY int) []Rock {
+	rocks := inp
+	columnIndexes := [][]int{}
 	for x := 0; x <= maxX; x = x + 1 {
 		columnIndexes = append(columnIndexes, []int{})
 		for i, rk := range rocks {
@@ -68,6 +73,7 @@ func RollRocksNorth(inp []Rock) []Rock {
 			}
 		}
 		endpoint := 0
+
 		for _, rks := range orderedRocks {
 			if rks.Mobile == false {
 				endpoint = rks.Y + 1
@@ -78,6 +84,151 @@ func RollRocksNorth(inp []Rock) []Rock {
 		}
 	}
 	return rocks
+}
+
+func RollRocksSouth(inp []Rock, maxX int, maxY int) []Rock {
+	rocks := inp
+	columnIndexes := [][]int{}
+	for x := 0; x <= maxX; x = x + 1 {
+		columnIndexes = append(columnIndexes, []int{})
+		for i, rk := range rocks {
+			if rk.X == x {
+				columnIndexes[x] = append(columnIndexes[x], i)
+			}
+		}
+	}
+
+	for _, col := range columnIndexes {
+		orderedRocks := []*Rock{}
+		for _, i := range col {
+			if len(orderedRocks) == 0 {
+				orderedRocks = append(orderedRocks, &rocks[i])
+			} else {
+				for ii := range orderedRocks {
+					if len(orderedRocks)-1 == ii {
+						orderedRocks = append([]*Rock{&rocks[i]}, orderedRocks...)
+						break
+					}
+					if orderedRocks[ii].Y > rocks[i].Y {
+						orderedRocks = append(orderedRocks, &rocks[i])
+						break
+					} else {
+
+						continue
+					}
+				}
+			}
+		}
+		endpoint := maxY
+		for _, rks := range orderedRocks {
+			if rks.Mobile == false {
+				endpoint = rks.Y - 1
+			} else {
+				rks.Y = endpoint
+				endpoint = endpoint - 1
+			}
+		}
+	}
+	return rocks
+}
+
+func RollRocksWest(inp []Rock, maxX int, maxY int) []Rock {
+	rocks := inp
+	columnIndexes := [][]int{}
+	for y := 0; y <= maxY; y = y + 1 {
+		columnIndexes = append(columnIndexes, []int{})
+		for i, rk := range rocks {
+			if rk.Y == y {
+				columnIndexes[y] = append(columnIndexes[y], i)
+			}
+		}
+	}
+
+	for _, col := range columnIndexes {
+		orderedRocks := []*Rock{}
+		for _, i := range col {
+			if len(orderedRocks) == 0 {
+				orderedRocks = append(orderedRocks, &rocks[i])
+			} else {
+				for ii := range orderedRocks {
+					if orderedRocks[ii].X < rocks[i].X {
+						if len(orderedRocks)-1 == ii {
+							orderedRocks = append(orderedRocks, &rocks[i])
+						}
+						continue
+					} else {
+						orderedRocks = append([]*Rock{&rocks[i]}, orderedRocks...)
+					}
+				}
+			}
+		}
+		endpoint := 0
+		for _, rks := range orderedRocks {
+			if rks.Mobile == false {
+				endpoint = rks.X + 1
+			} else {
+				rks.X = endpoint
+				endpoint = endpoint + 1
+			}
+		}
+	}
+	return rocks
+}
+
+func RollRocksEast(inp []Rock, maxX int, maxY int) []Rock {
+	rocks := inp
+	columnIndexes := [][]int{}
+	for y := 0; y <= maxY; y = y + 1 {
+		columnIndexes = append(columnIndexes, []int{})
+		for i, rk := range rocks {
+			if rk.Y == y {
+				columnIndexes[y] = append(columnIndexes[y], i)
+			}
+		}
+	}
+
+	for _, col := range columnIndexes {
+		orderedRocks := []*Rock{}
+		for _, i := range col {
+			if len(orderedRocks) == 0 {
+				orderedRocks = append(orderedRocks, &rocks[i])
+			} else {
+				for ii := range orderedRocks {
+					if len(orderedRocks)-1 == ii {
+						orderedRocks = append([]*Rock{&rocks[i]}, orderedRocks...)
+						break
+					}
+					if orderedRocks[ii].X > rocks[i].X {
+						orderedRocks = append(orderedRocks, &rocks[i])
+						break
+					} else {
+
+						continue
+					}
+				}
+			}
+		}
+		endpoint := maxX
+		for _, rks := range orderedRocks {
+			if rks.Mobile == false {
+				endpoint = rks.X - 1
+			} else {
+				rks.X = endpoint
+				endpoint = endpoint - 1
+			}
+		}
+	}
+	return rocks
+}
+
+// screw it
+
+func SpinCycle(inp []Rock, maxX int, maxY int) []Rock {
+	inp2 := RollRocksNorth(inp, maxX, maxY)
+	inp3 := RollRocksWest(inp2, maxX, maxY)
+	inp4 := RollRocksSouth(inp3, maxX, maxY)
+	inp5 := RollRocksEast(inp4, maxX, maxY)
+	return inp5
 }
 
 func CalculateLoadOnNorth(rocks []Rock) int {
@@ -104,9 +255,9 @@ func main() {
 	buf, _ := os.ReadFile("data.txt")
 	input := string(buf)
 
-	strt := ParseRocks(input)
+	strt, maxX, maxY := ParseRocks(input)
 
-	res := RollRocksNorth(strt)
+	res := RollRocksNorth(strt, maxX, maxY)
 	calc := CalculateLoadOnNorth(res)
 
 	fmt.Printf("Result: %d\n", calc)
--- 14/main_test.go
+++ 14/main_test.go
@@ -13,7 +13,44 @@ O.#..O.#.#
 ..O..#O..O
 .......O..
 #....###..
-#OO..#....`
+#OO..#....
+`
+
+var expectedRolledSouth string = `.....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O
+`
+
+var expectedRolledWest string = `O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#....
+`
+
+var expectedRolledEast string = `....O#....
+.OOO#....#
+.....##...
+.OO#....OO
+......OO#.
+.O#...O#.#
+....O#..OO
+.........O
+#....###..
+#..OO#....
+`
 
 var expectedRolledNorth string = `OOOO.#.O..
 OO..#....#
@@ -24,10 +61,45 @@ O..#.OO...
 ..O..#.O.O
 ..O.......
 #....###..
-#....#....`
+#....#....
+`
+
+var oneCycle string = `.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+`
+var twoCycle string = `.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+`
+var threeCycle string = `.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+`
 
 func TestParseRocks(t *testing.T) {
-	res := ParseRocks(exampleStart)
+	res, _, _ := ParseRocks(exampleStart)
 	if len(res) != 35 {
 		t.Errorf("Expected 35 got %v", len(res))
 	}
@@ -70,26 +142,93 @@ func PrintRocksAsMap(rocks []Rock) string {
 
 func TestRollRocksNorth(t *testing.T) {
 
-	strt := ParseRocks(exampleStart)
+	strt, maxX, maxY := ParseRocks(exampleStart)
 
-	res := RollRocksNorth(strt)
+	res := RollRocksNorth(strt, maxX, maxY)
 
 	mp := PrintRocksAsMap(res)
 
 	if mp != expectedRolledNorth {
-		// this works, I don't know why they aren't equal
-		//t.Errorf("Expected:\n%s\n got:\n%s\n", expectedRolledNorth, mp)
+		t.Errorf("Expected:\n%s\n got:\n%s\n", expectedRolledNorth, mp)
+	}
+}
+
+func TestRollRocksSouth(t *testing.T) {
+
+	strt, maxX, maxY := ParseRocks(exampleStart)
+
+	res := RollRocksSouth(strt, maxX, maxY)
+
+	mp := PrintRocksAsMap(res)
+
+	if mp != expectedRolledSouth {
+		t.Errorf("Expected:\n%s\n got:\n%s\n", expectedRolledSouth, mp)
+	}
+}
+
+func TestRollRocksWest(t *testing.T) {
+
+	strt, maxX, maxY := ParseRocks(exampleStart)
+
+	res := RollRocksWest(strt, maxX, maxY)
+
+	mp := PrintRocksAsMap(res)
+
+	if mp != expectedRolledWest {
+		t.Errorf("Expected:\n%s\n got:\n%s\n", expectedRolledWest, mp)
+	}
+}
+
+func TestRollRocksEast(t *testing.T) {
+
+	strt, maxX, maxY := ParseRocks(exampleStart)
+
+	res := RollRocksEast(strt, maxX, maxY)
+
+	mp := PrintRocksAsMap(res)
+
+	if mp != expectedRolledEast {
+		t.Errorf("Expected:\n%s\n got:\n%s\n", expectedRolledEast, mp)
 	}
 }
 
 func TestCalculateLoadOnNorth(t *testing.T) {
 
-	strt := ParseRocks(exampleStart)
+	strt, maxX, maxY := ParseRocks(exampleStart)
 
-	res := RollRocksNorth(strt)
+	res := RollRocksNorth(strt, maxX, maxY)
 	calc := CalculateLoadOnNorth(res)
 	if calc != 136 {
 
 		t.Errorf("Expected 136 got %v", calc)
+	}
+}
+
+func TestSpinCycle(t *testing.T) {
+
+	strt, maxX, maxY := ParseRocks(exampleStart)
+
+	res := SpinCycle(strt, maxX, maxY)
+
+	mp := PrintRocksAsMap(res)
+
+	if mp != oneCycle {
+		t.Errorf("Expected:\n%s\n got:\n%s\n", oneCycle, mp)
+	}
+
+	res = SpinCycle(res, maxX, maxY)
+
+	mp = PrintRocksAsMap(res)
+
+	if mp != twoCycle {
+		t.Errorf("Expected:\n%s\n got:\n%s\n", twoCycle, mp)
+	}
+
+	res = SpinCycle(res, maxX, maxY)
+
+	mp = PrintRocksAsMap(res)
+
+	if mp != threeCycle {
+		t.Errorf("Expected:\n%s\n got:\n%s\n", threeCycle, mp)
 	}
 }
