5d1e8b158d800c497d69ba187e6e06e26a1e8673
Lee Martin <hello@leejohnmartin.co.uk>
2023-12-15 19:59:13 +0000

    15-pt2
    Lesson: Maps don't have consistent order
    for range in go

--- 15/main.go
+++ 15/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Lens struct {
+	Label   string
+	Stength int
+}
 
 func CalculateHash(input string) int {
 	cuml := 0
@@ -12,6 +18,52 @@ func CalculateHash(input string) int {
 		cuml = cuml + int(ch)
 		cuml = cuml * 17
 		cuml = cuml % 256
+	}
+	return cuml
+}
+
+// returns boxes,
+func PerformOperationOnBoxes(boxes map[int][]Lens, op string) map[int][]Lens {
+
+	if strings.Index(op, "-") != -1 { // - : remove
+		lbl := strings.TrimSuffix(op, "-")
+		hsh := CalculateHash(lbl)
+		indx := -1
+		for i, lns := range boxes[hsh] {
+			if lns.Label == lbl {
+				indx = i
+			}
+		}
+		if indx > -1 {
+			boxes[hsh] = append(boxes[hsh][:indx], boxes[hsh][indx+1:]...)
+		}
+	} else { // = : add to end, or if already in box, replace strength
+		prts := strings.Split(op, "=")
+		lbl := prts[0]
+		hsh := CalculateHash(lbl)
+		strg, _ := strconv.Atoi(prts[1])
+
+		indx := -1
+		for i, lns := range boxes[hsh] {
+			if lns.Label == lbl {
+				indx = i
+			}
+		}
+		if indx > -1 {
+			boxes[hsh][indx].Stength = strg
+		} else {
+			boxes[hsh] = append(boxes[hsh], Lens{lbl, strg})
+		}
+	}
+	return boxes
+}
+
+func CalculateValueFromBoxes(boxes map[int][]Lens) int {
+	cuml := 0
+	for bxnum, lenses := range boxes {
+		for i, lns := range lenses {
+			cuml = cuml + ((1 + bxnum) * (i + 1) * lns.Stength)
+		}
 	}
 	return cuml
 }
@@ -24,4 +76,11 @@ func main() {
 		cuml = cuml + CalculateHash(com)
 	}
 	fmt.Printf("Result: %v\n", cuml)
+
+	boxes := map[int][]Lens{}
+	for _, com := range strings.Split(input, ",") {
+		boxes = PerformOperationOnBoxes(boxes, com)
+	}
+
+	fmt.Printf("Result: %v\n", CalculateValueFromBoxes(boxes))
 }
--- 15/main_test.go
+++ 15/main_test.go
@@ -23,3 +23,46 @@ func TestAssert(t *testing.T) {
 		}
 	}
 }
+func TestOperations(t *testing.T) {
+	// just ignore the numbers
+	exps := []string{
+		"rn=1",
+		"qp=3",
+		"cm=2",
+		"qp-",
+		"pc=4",
+		"ot=9",
+		"ab=5",
+		"pc-",
+		"pc=6",
+		"ot=7",
+	}
+	actual := map[int][]Lens{}
+	exp := map[int][]Lens{
+		0: {{"rn", 1}, {"cm", 2}},
+		3: {{"ot", 7}, {"ab", 5}, {"pc", 6}},
+	}
+	for _, op := range exps {
+		actual = PerformOperationOnBoxes(actual, op)
+	}
+
+	for k, bx := range exp {
+		for i, b := range bx {
+			if actual[k][i].Label != exp[k][i].Label ||
+				actual[k][i].Stength != exp[k][i].Stength {
+
+				t.Errorf("%v %v - Expected: %v got: %v", k, i, b, actual[k][i])
+			}
+
+			if len(actual[k]) != len(exp[k]) {
+				t.Errorf("box %v - Too many items", k)
+
+			}
+		}
+	}
+
+	res := CalculateValueFromBoxes(actual)
+	if res != 145 {
+		t.Errorf("Expected 145 got: %v", res)
+	}
+}
