1f409d22af5152fb238ce5bbb22ad99874c713ce
Lee Martin <hello@leejohnmartin.co.uk>
2023-12-21 00:31:58 +0000

    20-pt1
    I am a god amongst men

--- 20/main.go
+++ 20/main.go
@@ -3,38 +3,185 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 type ModuleType int64
 
-const (
-	Broadcaster ModuleType = 0
-	FlipFlop    ModuleType = 1
-	Conjunction ModuleType = 2
-)
+type Broadcaster struct {
+	Name      string
+	SendsTo   []string
+	HighQueue []bool
+}
 
-type CommsModule struct {
+type FlipFlop struct {
+	Name      string
+	SendsTo   []string
+	State     bool
+	HighQueue []bool
+}
+
+type Conjunction struct {
 	Name    string
+	Memory  map[string]bool
 	SendsTo []string
-	Type    ModuleType
 }
 
-func (cms CommsModule) SendPulse(pulse bool) {
-
+type CommModule interface {
+	ReceivePulse(pulse bool, sender string)
+	SendPulseTo() (bool, []string)
 }
 
-func (cms CommsModule) ProcessPulses() {
-
+func (cms *Broadcaster) ReceivePulse(pulse bool, sender string) {
+	cms.HighQueue = append(cms.HighQueue, pulse)
 }
 
-func ParseCommsModule()
+func (cms *Broadcaster) SendPulseTo() (bool, []string) {
+	pls := false
+	if len(cms.HighQueue) == 0 {
+		return false, []string{}
+	}
+	pls, cms.HighQueue = cms.HighQueue[0], cms.HighQueue[1:]
+	return pls, cms.SendsTo
+}
 
-func myTestFunction(input string) int {
-	return 1
+// dule receives a low pulse, it flips between on and off.
+// If it was off, it turns on and sends a high pulse.
+// If it was on, it turns off and sends a low pulse.
+func (cms *FlipFlop) ReceivePulse(pulse bool, sender string) {
+	if pulse {
+		//noop
+		return
+	}
+	cms.State = !cms.State
+	cms.HighQueue = append(cms.HighQueue, cms.State)
+}
+
+func (cms *FlipFlop) SendPulseTo() (bool, []string) {
+	pls := false
+	if len(cms.HighQueue) == 0 {
+		return false, []string{}
+	}
+	pls, cms.HighQueue = cms.HighQueue[0], cms.HighQueue[1:]
+	return pls, cms.SendsTo
+}
+
+func (cms *Conjunction) ReceivePulse(pulse bool, sender string) {
+	cms.Memory[sender] = pulse
+}
+
+func (cms *Conjunction) SendPulseTo() (bool, []string) {
+	for _, high := range cms.Memory {
+		if !high {
+			return true, cms.SendsTo
+		}
+	}
+	return false, cms.SendsTo
+}
+
+func ParseCommsModules(input string) map[string]CommModule {
+	// we need to make a "inverse" of it because of the memory thing
+	res := map[string]CommModule{}
+	receivesFrom := map[string][]string{}
+	for _, str := range strings.Split(input, "\n") {
+		firstSpaceIndex := strings.Index(str, " ")
+		name := str[:firstSpaceIndex]
+		if name[0] != 'b' {
+			name = name[1:]
+		}
+		targets := strings.Split(str[firstSpaceIndex+1+3:], ", ")
+		for _, tgt := range targets {
+			_, ok := receivesFrom[tgt]
+			if !ok {
+				receivesFrom[tgt] = []string{name}
+			} else {
+				receivesFrom[tgt] = append(receivesFrom[tgt], name)
+			}
+		}
+	}
+	for _, str := range strings.Split(input, "\n") {
+		firstSpaceIndex := strings.Index(str, " ")
+		name := str[:firstSpaceIndex]
+		typ := name[0]
+		if name[0] != 'b' {
+			name = name[1:]
+		}
+		targets := strings.Split(str[firstSpaceIndex+1+3:], ", ")
+		if typ == 'b' {
+			// broadcaster
+			res[name] = &Broadcaster{
+				Name:    name,
+				SendsTo: targets,
+			}
+		}
+		if typ == '&' {
+			// conjunction
+			mem := map[string]bool{}
+			for _, k := range receivesFrom[name] {
+				mem[k] = false
+			}
+			res[name] = &Conjunction{
+				Name:    name,
+				SendsTo: targets,
+				Memory:  mem,
+			}
+		}
+		if typ == '%' {
+			// flipflop
+			res[name] = &FlipFlop{
+				Name:    name,
+				SendsTo: targets,
+				State:   false,
+			}
+		}
+	}
+	return res
+}
+
+type Pulse struct {
+	Target string
+	Sender string
+	High   bool
+}
+
+// lowpulses, highpulses
+func HitButton(network map[string]CommModule) (int, int) {
+	queue := []string{"broadcaster"}
+	lowCount := 1 //start at 1 from button
+	network["broadcaster"].ReceivePulse(false, "button")
+	highCount := 0
+	query := ""
+	for len(queue) > 0 {
+		query, queue = queue[0], queue[1:]
+		if network[query] == nil {
+			continue
+		}
+		high, tgts := network[query].SendPulseTo()
+		for _, tgt := range tgts {
+			if high {
+				highCount = highCount + 1
+			} else {
+				lowCount = lowCount + 1
+			}
+			queue = append(queue, tgt)
+			if network[tgt] != nil {
+				network[tgt].ReceivePulse(high, query)
+			}
+		}
+	}
+	return lowCount, highCount
 }
 
 func main() {
 	buf, _ := os.ReadFile("data.txt")
-	stringput := string(buf)
-	fmt.Printf("Result: %v", myTestFunction(stringput))
+	input := string(buf)
+
+	ntwrk := ParseCommsModules(input)
+	low, high := 0, 0
+	for i := 0; i < 1000; i = i + 1 {
+		lowAdd, highAdd := HitButton(ntwrk)
+		low = lowAdd + low
+		high = highAdd + high
+	}
+	fmt.Printf("Part One: %v\n", low*high)
 }
--- 20/main_test.go
+++ 20/main_test.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
+
+// % -- flip flop
+// starts off
+// high pulse -> do nothing
+// low pulse -> switch on/off
+// -- becomes on -> send high
+// -- becomes off -> send low
+
+// & -- Conjunction
+// starts remembering low for all inputs
+// When a pulse is received,
+// the conjunction module first updates its memory for that input to received
+// Then, if it remembers high pulses for all inputs, it sends a low pulse;
+// otherwise, it sends a high pulse.
+
+// broadcaster -- what it is
+// When it receives a pulse, it sends the same pulse to all of its destination modules
 
 var exampleOne = `broadcaster -> a, b, c
 %a -> b
@@ -17,10 +33,33 @@ var exampleTwo = `broadcaster -> a
 %b -> con
 &con -> output`
 
-func TestAssert(t *testing.T) {
-	res := myTestFunction("lol")
-	fmt.Printf(exampleOne)
-	if res != 2 {
-		t.Errorf("Expected 2 got %v", res)
+func TestTheWholeDamnThing(t *testing.T) {
+	// Sometimes, you just do the do
+	ntwrk := ParseCommsModules(exampleOne)
+	low, high := HitButton(ntwrk)
+
+	if low != 8 {
+		t.Errorf("Expected 8 got %v", low)
+	}
+	if high != 4 {
+		t.Errorf("Expected 4 got %v", high)
+	}
+}
+
+func TestTheWholeDamnThingTwo(t *testing.T) {
+	// Sometimes, you just do the do
+	ntwrk := ParseCommsModules(exampleTwo)
+	low, high := 0, 0
+	// 1000 times, 4250 low pulses and 2750 high pulses are sent. Multipl
+	for i := 0; i < 1000; i = i + 1 {
+		lowAdd, highAdd := HitButton(ntwrk)
+		low = lowAdd + low
+		high = highAdd + high
+	}
+	if low != 4250 {
+		t.Errorf("Expected 4250 got %v", low)
+	}
+	if high != 2750 {
+		t.Errorf("Expected 2750 got %v", high)
 	}
 }
